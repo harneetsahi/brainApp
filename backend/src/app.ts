541e5868b4100a7468e0006c7aeb4b7f24c36d3b
Harneet <harneetsahi8@gmail.com>
2025-05-27 14:53:28 -0500

    add: cors and cookie parser setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,24 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : ["https://localhost:5173"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 import userRouter from "./routes/user.routes";
 
-app.use("/api/v1/users", userRouter);
+app.use("/api/v1", userRouter);
 
 export { app };
