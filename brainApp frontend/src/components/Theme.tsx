5d807c8e8fee9a82dea121eb0dae576c025d2b7a
Harneet <harneetsahi8@gmail.com>
2025-05-29 15:47:50 -0500

    add: theme in localstorage

--- brainApp frontend/src/components/Theme.tsx
+++ brainApp frontend/src/components/Theme.tsx
@@ -3,10 +3,21 @@ import { SunIcon } from "../icons/SunIcon";
 import { MoonIcon } from "../icons/MoonIcon";
 
 export const Theme = ({ text }: { text: string }) => {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark";
+  });
 
   useEffect(() => {
-    document.querySelector("html")?.classList.toggle("dark");
+    const htmlEl = document.documentElement;
+
+    if (darkMode) {
+      htmlEl.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      htmlEl.classList.remove("dark");
+      localStorage.setItem("theme", "");
+    }
   }, [darkMode]);
 
   return (
@@ -15,8 +26,8 @@ export const Theme = ({ text }: { text: string }) => {
         onClick={() => setDarkMode(!darkMode)}
         className="cursor-pointer flex gap-3 items-center font-sans text-sm p-2 rounded-lg hover:bg-zinc-100 dark:hover:bg-zinc-700 text-gray-800 dark:text-gray-300 "
       >
-        {darkMode ? <SunIcon size={"md"} /> : <MoonIcon size={"md"} />}
-        {text}
+        {darkMode ? <SunIcon size="size-4" /> : <MoonIcon size="size-4" />}
+        <p className="hidden sm:inline">{text}</p>
       </button>
     </>
   );
