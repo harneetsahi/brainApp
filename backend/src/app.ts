fb54ff9f2bdea19c53f47b50347923b1b4352a51
Harneet <harneetsahi8@gmail.com>
2025-05-30 13:29:23 -0500

    fix: url error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,9 +9,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin:
-      process.env.NODE_ENV === "production"
-        ? false
-        : ["https://localhost:5173"],
+      process.env.NODE_ENV === "production" ? false : ["http://localhost:5173"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
