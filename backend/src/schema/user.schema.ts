8bed104be3e4dc30b6d42ad5ee1f7b88be2d687c
Harneet <harneetsahi8@gmail.com>
2025-05-31 17:08:14 -0500

    add: timestamps

--- backend/src/schema/user.schema.ts
+++ backend/src/schema/user.schema.ts
@@ -1,27 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export const User = mongoose.model("User", userSchema);
