50f3003c9f69e2156a2f82945e66ba44bf7ec4ae
Harneet <harneetsahi8@gmail.com>
2025-06-01 17:25:46 -0500

    add: input element for auth pages

---
+++ brainApp frontend/src/components/AuthInput.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+interface InputProps {
+  icon?: React.JSX.Element;
+  placeholder: string;
+  type: string;
+  name: string;
+  id: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value?: string;
+  minLength?: number;
+  maxLength?: number;
+  pattern?: string;
+  title?: string;
+  required?: boolean;
+  className?: string;
+}
+
+export function AuthInput({
+  icon,
+  placeholder,
+  type,
+  name,
+  id,
+  onChange,
+  value,
+  minLength,
+  maxLength,
+  pattern,
+  title,
+  required,
+  className,
+}: InputProps) {
+  return (
+    <>
+      <div
+        className={`border-1 border-zinc-200/40 dark:border-zinc-700 dark:bg-zinc-800/40 bg-zinc-50/50 rounded-md text-sm w-full flex items-center gap-2 ${className}`}
+      >
+        <label className="pl-2 absolute ">{icon}</label>
+        <input
+          className="pl-8 py-2 flex-1"
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          id={id}
+          onChange={onChange}
+          value={value}
+          minLength={minLength}
+          maxLength={maxLength}
+          pattern={pattern}
+          title={title}
+          required={required}
+        />
+      </div>
+    </>
+  );
+}
