39ead04a1800c6a281f713571ba19e92e1a2e449
Harneet <harneetsahi8@gmail.com>
2025-06-02 17:38:31 -0500

    add: functionality to add notes

--- brainApp frontend/src/components/CreateContentModal.tsx
+++ brainApp frontend/src/components/CreateContentModal.tsx
@@ -1,10 +1,8 @@
-import { CrossIcon } from "../icons/CrossIcon";
+import { useForm } from "react-hook-form";
+import { IContent } from "../store_and_types/types";
 import { Button } from "./Button";
-import { InputDiv } from "./InputComponent";
-
-const labelStyle = "flex flex-col  text-gray-300 mb-1 capitalize";
-const inputStyle =
-  "w-full bg-gray-200 px-3 py-2.5 text-gray-800 rounded-lg mb-3 font-light ";
+import { InputDiv } from "./InputDiv";
+import { usePostContents } from "../hooks/useContentQueries";
 
 interface CreateContentProps {
   isOpen: boolean;
@@ -12,54 +10,63 @@ interface CreateContentProps {
 }
 
 export const CreateContentModal = ({ isOpen, onClose }: CreateContentProps) => {
+  const { register, handleSubmit, reset } = useForm<IContent>({
+    defaultValues: {
+      title: "",
+      link: "",
+      description: "",
+    },
+  });
+
+  const { mutate: postContent } = usePostContents();
+
+  const addContent = (data: IContent) => {
+    try {
+      postContent(data);
+      reset();
+      onClose();
+    } catch (error) {
+      console.log("failed to post ", error);
+    }
+  };
+
   return (
     <>
       {isOpen && (
-        <div className="w-screen h-screen bg-zinc-700/90 fixed top-0 left-0">
-          <article className=" flex justify-center items-center h-full  ">
-            <form
-              className="bg-zinc-800 min-w-80 px-7 pt-4 pb-8 flex flex-col rounded-xl "
-              action="#"
-            >
-              <a
-                onClick={onClose}
-                className="self-end cursor-pointer hover:bg-zinc-700 rounded-md p-1"
-              >
-                <CrossIcon size={"lg"} />
-              </a>
+        <article className=" flex justify-center items-center">
+          <form
+            className={`py-5 dark:bg-zinc-800/50  bg-white shadow-md dark:text-white text-zinc-800 sm:w-[40vw] w-68   px-5 flex flex-col gap:2  sm:text-md text-sm border-1 border-t-0 dark:border-zinc-700 border-zinc-50 ${
+              isOpen ? "rounded-b-lg" : "rounded-lg"
+            } `}
+            onSubmit={handleSubmit(addContent)}
+          >
+            <InputDiv
+              className="text-lg"
+              placeholder={"Title"}
+              name="title"
+              register={register}
+            />
 
-              <InputDiv text={"title"} />
+            <InputDiv
+              placeholder={"Link (optional)"}
+              name="link"
+              register={register}
+              className=""
+            />
+            <InputDiv
+              placeholder={"Note"}
+              name="description"
+              register={register}
+            />
 
-              <div>
-                <label className={labelStyle} htmlFor="type">
-                  type
-                </label>
-                <select name="selectedType" id="" className={inputStyle}>
-                  <option selected value="">
-                    Choose one :
-                  </option>
-                  <option value="twitter">Tweet</option>
-                  <option value="youtube">Youtube video</option>
-                  <option value="other">Other</option>
-                </select>
-              </div>
-
-              <InputDiv text={"link"} />
-              <InputDiv text={"description"} />
-
-              <div className=" self-center mt-3 ">
-                <Button
-                  variant="tertiary"
-                  size="sm"
-                  text="Add"
-                  className={
-                    "hover:bg-zinc-100 hover:ring-1 hover:ring-zinc-100 "
-                  }
-                />
-              </div>
-            </form>
-          </article>
-        </div>
+            <Button
+              variant="tertiary"
+              size="sm"
+              text="Add"
+              className="px-6 bg-zinc-100 dark:bg-zinc-800 shadow-xs border-1 dark:border-zinc-700/50 border-zinc-100 self-center mt-3"
+            />
+          </form>
+        </article>
       )}
     </>
   );
