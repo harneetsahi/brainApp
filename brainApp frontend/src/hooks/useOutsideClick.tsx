749179980d708dec0e919706d25efa88cb6dd02c
Harneet <harneetsahi8@gmail.com>
2025-06-03 14:59:45 -0500

    add: close on outside click hook

---
+++ brainApp frontend/src/hooks/useOutsideClick.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+const useOutsideClick = (
+  ref: React.RefObject<HTMLElement> | React.RefObject<null>,
+  callback: () => void
+) => {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) callback();
+    };
+
+    document.addEventListener("click", handleClick);
+
+    return () => {
+      document.removeEventListener("click", handleClick);
+    };
+  }, [ref, callback]);
+};
+
+export { useOutsideClick };
