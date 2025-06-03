0ad10fe9ae4135c70f6b16eeacac8ff880d0898f
Harneet <harneetsahi8@gmail.com>
2025-06-03 14:54:31 -0500

    update: naming and styling

--- brainApp frontend/src/components/Button.tsx
+++ brainApp frontend/src/components/Button.tsx
@@ -1,26 +1,27 @@
 import { ReactElement } from "react";
 
 interface ButtonProps {
-  variant: "primary" | "secondary" | "tertiary" | "tertiaryDark";
+  variant: "primary" | "iconBtn" | "tertiary" | "tertiaryDark";
   size: "sm" | "md" | "lg";
   text?: string | ReactElement;
   startIcon?: ReactElement;
   className?: string;
   onClick?: () => void;
   disabled?: boolean;
+  type?: "submit" | "reset" | "button" | undefined;
 }
 
 const variantStyles = {
   primary:
-    "bg-zinc-800 border-1 border-zinc-600 text-zinc-100  hover:scale-103    ",
-  secondary:
-    "dark:bg-zinc-800 bg-white text-zinc-800 dark:text-zinc-50 border-1 border-zinc-200 dark:border-zinc-700 hover:scale-103 shadow-sm ",
-  tertiary: " text-zinc-800 dark:text-zinc-100  ",
+    "dark:bg-zinc-800 bg-white text-zinc-800 dark:text-zinc-50 border-1 border-zinc-200 dark:border-zinc-800 text-zinc-100  hover:scale-103  shadow-xs   ",
+  iconBtn:
+    "dark:bg-zinc-800 bg-white text-zinc-800 dark:text-zinc-50 border-1 border-zinc-200 dark:border-zinc-700 hover:scale-103 shadow-xs ",
+  tertiary: "bg-white dark:bg-zinc-800/50 text-zinc-800 dark:text-zinc-100  ",
   tertiaryDark:
-    "text-zinc-100 dark:text-zinc-800 bg-zinc-800 dark:bg-zinc-100 h-10   ",
+    "text-zinc-100 dark:text-zinc-800 bg-zinc-800 dark:bg-zinc-100 h-10  ",
 };
 
-const defaultStyles = "cursor-pointer hover:shadow-sm rounded-lg ";
+const defaultStyles = "cursor-pointer hover:shadow-xs rounded-lg ";
 
 const sizeVariant = {
   sm: "px-3 py-2 text-sm",
@@ -36,27 +37,30 @@ export const Button = ({
   className,
   onClick,
   disabled,
+  type,
 }: ButtonProps) => {
   return (
     <>
-      {variant === "tertiary" || variant === "tertiaryDark" ? (
+      {variant === "iconBtn" ? (
         <button
           className={`${variantStyles[variant]} ${defaultStyles} ${sizeVariant[size]} ${className}`}
           onClick={onClick}
           disabled={disabled}
+          type={type}
         >
           {startIcon}
-          {text}
+
+          <span className="hidden md:inline w-full ">{text}</span>
         </button>
       ) : (
         <button
           className={`${variantStyles[variant]} ${defaultStyles} ${sizeVariant[size]} ${className}`}
           onClick={onClick}
           disabled={disabled}
+          type={type}
         >
           {startIcon}
-
-          <span className="hidden sm:inline w-full">{text}</span>
+          {text}
         </button>
       )}
     </>
